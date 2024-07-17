d71a8f2d99465fa05e92125eb5375efc2a7b0fa6
Vaibhav <vscoderr@gmail.com>
2024-07-18 02:37:28 +0530

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector(".menu i");
+let cross = document.querySelector(".navlinks i");
+let tl = gsap.timeline()
+
+tl.to(".navlinks",{
+   width:"100%",
+   duration: 0.3
+})
+
+tl.from(".navlinks ul li",{
+   x: 100,
+   stagger: 0.3,
+   duration: 0.8,
+   opacity:0
+})
+
+tl.from(".navlinks i",{
+   opacity: 0
+})
+
+tl.pause()
+
+
+menu.addEventListener("click",() => {
+   tl.play()
+   
+})
+
+cross.addEventListener("click",() => {
+   tl.reverse()
+})
