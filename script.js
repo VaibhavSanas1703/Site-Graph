81c23cf28cd61d9d4029c7b7ab0e5f107f20a5d6
Vaibhav <vscoderr@gmail.com>
2024-07-19 17:57:51 +0530

    commit

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let tl = gsap.timeline()
 
 tl.to(".navlinks",{
    width:"100%",
-   duration: 0.3
+   duration: 0.1
 })
 
 tl.from(".navlinks ul li",{
    y: 100,
    stagger: 0.3,
-   duration: 0.8,
+   duration: 0.5,
    opacity:0
 })
 
@@ -20,8 +20,8 @@ tl.from(".navlinks i",{
 
 tl.pause()
 
-
 menu.addEventListener("click",() => {
+   
    tl.play()
    
 })
@@ -38,18 +38,26 @@ gsap.from(".home-left h4",{
 
 gsap.from(".home-left h1",{
    opacity:0,
-   duration: 3,
+   duration: 2,
 })
 gsap.from(".home-left p",{
    opacity:0,
-   duration: 3,
+   duration: 2,
 })
 gsap.from(".home-left a",{
    opacity:0,
-   duration: 3,
+   duration: 2,
 })
 
 gsap.from(".home-right img",{
    opacity:0,
    duration: 2,
 })
+
+let loaderCo = document.getElementById("loader-container");
+
+// window.addEventListener("load",() => {
+//    setTimeout(() => {
+//       loaderCo.classList.toggle("toggle-loader")
+//    },1000)
+// })
